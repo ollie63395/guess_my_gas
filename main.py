b437e3b6c7df5a3de28c03b66fb9e75e424d4154
Ollie <ollie.63395@gmail.com>
2025-06-12 10:38:21 +1000

    Update README.md

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ fuel_url = "https://www.7eleven.com.au/storelocator-retail/mulesoft/fuelPrices?s
 # CSV file path
 csv_file = "fuel_prices.csv"
 
+price_data = []
+
 def fetch_fuel_price():
     try:
         price_response = requests.get(fuel_url)
         price_data = price_response.json()
 
+        # index 5 corresponds to E95 (PULP) at Chadstone store
         index = 5
         # convert price to cents/L
         price = price_data['data'][index]['price'] / 10
@@ -27,14 +30,18 @@ def fetch_fuel_price():
             writer = csv.writer(file)
             writer.writerow(data)
 
+        price_data.append(data)
+
         print(f"{timestamp} - E95 (PULP) Price at Chadstone: {price:.1f}¢/L (Logged)")
 
     except Exception as e:
         print("Error fetching data: ", e)
 
 
-while True:
-    fetch_fuel_price()
-    print(f"Current time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    print("Waiting 3 hours before the next run...\n")
-    time.sleep(3 * 60 * 60)  # Sleep for 3 hours (in seconds)
+fetch_fuel_price()
+
+# while True:
+#     fetch_fuel_price()
+#     print(f"Current time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+#     print("Waiting 3 hours before the next run...\n")
+#     time.sleep(3 * 60 * 60)  # Sleep for 3 hours (in seconds)
